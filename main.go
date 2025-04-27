c4942bae2b32c081f6afb2450a548cc8d32dfc23
Lanre <lanre@Haliax0.local>
2025-04-27 11:57:47 -0500

    added hits counter and reset functionality

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync/atomic"
 
 	"github.com/AdonaIsium/chirpy/app"
 	"github.com/joho/godotenv"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getHitsCounter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	count := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, `Hits: %d`, count)
+}
+
+func (cfg *apiConfig) resetHitsCounter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte(`"Metrics reset"`))
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	fs := http.FileServer(http.Dir("."))
 	appFS := http.StripPrefix("/app/", fs)
 	if err := godotenv.Load(); err != nil {
@@ -21,9 +50,11 @@ func main() {
 		port = "8080"
 	}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", appFS)
-	mux.Handle("/", appFS)
-	mux.Handle("/assets", appFS)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appFS))
+	mux.Handle("/", apiCfg.middlewareMetricsInc(appFS))
+	mux.Handle("/assets/", apiCfg.middlewareMetricsInc(appFS))
+	mux.HandleFunc("/metrics", apiCfg.getHitsCounter)
+	mux.HandleFunc("/reset", apiCfg.resetHitsCounter)
 	mux.HandleFunc("/healthz", app.ServerReadiness)
 
 	addr := ":" + port
