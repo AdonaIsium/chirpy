4b14305b8a9e06fa70378076d5b3e5b3ab3862e4
Lanre <lanre@Haliax0.local>
2025-04-26 23:26:05 -0500

    wow, just wow

---
+++ app/healthz.go
@@ -0,0 +1,9 @@
+package app
+
+import "net/http"
+
+func ServerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`"OK"`))
+}
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/AdonaIsium/chirpy/app"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("."))
+	appFS := http.StripPrefix("/app/", fs)
 	if err := godotenv.Load(); err != nil {
 		log.Println("warning, no .env file found")
 	}
@@ -19,11 +21,10 @@ func main() {
 		port = "8080"
 	}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
-	})
+	mux.Handle("/app/", appFS)
+	mux.Handle("/", appFS)
+	mux.Handle("/assets", appFS)
+	mux.HandleFunc("/healthz", app.ServerReadiness)
 
 	addr := ":" + port
 
