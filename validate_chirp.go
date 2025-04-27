6b7d1b5df456ec645987cdd1095496859719a3a2
Lanre <lanre@Haliax0.local>
2025-04-27 16:39:55 -0500

    added some profanity checking

--- validate_chirp.go
+++ validate_chirp.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Chirp struct {
@@ -13,7 +14,7 @@ type ErrorResp struct {
 	Error string `json:"error"`
 }
 type ValidResp struct {
-	Valid bool `json:"valid"`
+	CleanedBody string `json:"cleaned_body"`
 }
 
 func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
@@ -22,34 +23,46 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&chirp)
 	if err != nil {
 		log.Printf("Error decoding chirp: %s", err)
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	if len(chirp.Body) > 140 {
-		log.Printf("Chirp must be less than 140 chars. Receied %d", len(chirp.Body))
+		log.Printf("Chirp must be less than 140 chars. Received %d", len(chirp.Body))
 		errorResp := ErrorResp{
 			Error: "Chirp is too long",
 		}
 		jsonResp, err := json.Marshal(errorResp)
 		if err != nil {
 			log.Printf("Error encoding error: %s", err)
-			w.WriteHeader(500)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonResp)
 		return
 	}
 
-	validResp := ValidResp{
-		Valid: true,
+	splitChirp := strings.Split(chirp.Body, " ")
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for i, word := range splitChirp {
+		for _, profanity := range profaneWords {
+			if strings.ToLower(word) == profanity {
+				splitChirp[i] = "****"
+			}
+		}
 	}
+	cleanedBody := strings.Join(splitChirp, " ")
+
+	validResp := ValidResp{
+		CleanedBody: cleanedBody,
+	}
+
 	jsonResp, err := json.Marshal(validResp)
 	if err != nil {
 		log.Printf("Error encoding valid: %s", err)
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
